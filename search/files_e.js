ae928be82f14c9a5c5f9ffd385ebe3a9672d021a
Dragos Bogdan <dragos.bogdan@analog.com>
2017-11-08 14:44:44 +0200

    Update documentation to 0a656a2

---
+++ search/files_e.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['rtc_2ec_16885',['rtc.c',['../rtc_8c.html',1,'']]],
+  ['rtc_2eh_16886',['rtc.h',['../rtc_8h.html',1,'']]],
+  ['rtc_5fextra_2eh_16887',['rtc_extra.h',['../rtc__extra_8h.html',1,'']]]
+];
